92c01267d4c29a0b1ad7b358d433edc66810e273
engalv <tangbosin5@gmail.com>
2026-02-03 16:48:41 +0100

    Comments and empty functions

--- client/shared/gamestate.js
+++ client/shared/gamestate.js
@@ -1,15 +1,32 @@
-
+import { Board } from "./board.js"
 
 export class GameState extends EventTarget {
 
-    constructor() {
+
+    // Tänker här har vi funktioner, 
+    constructor(board) {
         super()
+        this.board = board;
         this.currentPlayer = 0; // 0 = virus 1 = antivirus? Virus startar?
         this.gameOver = false;
+        this.turnTimer = null;
 
     }
 
-    checkWin() {
+    // CheckWin bör kolla om Virus har giltiga drag när det är Virus' tur, om AntiVirus inte har inga giltiga drag
+    // kanske man skippar dess tur?
+    // Skapa 
+    checkWin() { // Ska väl kolla om virus har giltiga drag och det är virus' tur
+
+        // this.board.virus.getValidMoves(); // Virus är egentligen en privat variabel enligt UML, detta ska inte gå
+        // this.board.virus.getCoveredServerCount(); // Samma sak som ovan,
+        // Detta är win conditions för respektive spelare
+
+
+    }
+
+    // 
+    changeTurn() {
 
     }
 
@@ -18,4 +35,7 @@ export class GameState extends EventTarget {
             return
         }
     }  
+
+
+
 }
