2091e6407cd6637520173294b86d95c688f4977b
weraf <104367292+weraf@users.noreply.github.com>
2026-02-09 14:28:41 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ function newConnection(socket) {
     users.push(newUser)
     newUser.on(ACTIONS.DISCONNECT,userDisconnected.bind(this, newUser))
     newUser.on(ACTIONS.FIND_GAME,(queueType) => {lobbyHandler.addUserToQueue(newUser,queueType)})
+    newUser.on(ACTIONS.STOP_FINDING_GAME, () => {lobbyHandler.removeUserFromQueue(newUser)})
 }
 
 function userDisconnected(user, reason) {
