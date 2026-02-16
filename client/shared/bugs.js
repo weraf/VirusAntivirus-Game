691eb2c9aaac91a8da91d4776389ea29e09a03c9
weraf <104367292+weraf@users.noreply.github.com>
2026-02-16 16:18:31 +0100

    Beginning of bug-handling backend

--- client/shared/bugs.js
+++ client/shared/bugs.js
@@ -37,30 +37,27 @@ export class Bugs extends EventTarget {
         return startNodes;
     }
 
-    respawnBugAtNode(node, newNode = null) {
-        let bugIndex = this.nodes.findIndex((n) => {return n == node;});
+    respawnBugAtNode(oldNode, newNode = null) {
+        let bugIndex = this.nodes.findIndex((n) => {return n == oldNode;});
         if (bugIndex == -1) {
             return false // There wasn't a bug at this node
         }
         if (newNode === null) {
             // New node wasn't set, pick a random one
             // Not so random random. Should be replaced by random pick later.
-            let randomNode = node;
-            for (let n = 0; n < 20; n++) {
-                for (let neighbor of randomNode.neighbors) {
-                    // Move to a random neighbor
-                    if ((this.board.isNodeEmpty(neighbor) && !neighbor.isServer()) || n < 10) {
-                        randomNode = neighbor;
-                        break;
-                    }
-                }
-            }
-            newNode = randomNode;
+            newNode = this.pickRandomNode(oldNode);
         }
+
         this.nodes[bugIndex] = newNode; // Replace the last bug
-        //this.createBugAtRandom();
-        this.dispatchEvent(new Event(Bugs.EVENTS.BUG_MOVED));
-        return true;
+        
+        this.dispatchEvent(new CustomEvent(Bugs.EVENTS.BUG_MOVED, {
+            detail: {
+                from: oldNode.id,
+                to: newNode.id
+            }
+        }));
+
+        return { from: oldNode.id, to: newNode.id };
     }
 
     /**
@@ -80,5 +77,18 @@ export class Bugs extends EventTarget {
             return; // Can't create bug on a non-empty node. This will also stop it from create two bugs on the same node
         }
         this.nodes.push(node);
+    }
+
+    pickRandomNode(startNode) {
+        let randomNode = startNode;
+        for (let i = 0; i < 20; i++) {
+            for (let neighbor of randomNode.neighbors) {
+                // Move to a random neighbor
+                if (this.board.isNodeEmpty(neighbor) && !neighbor.isServer() || i < 10) {
+                    return neighbor;
+                }
+            }
+        }
+        return startNode;
     }
 }
